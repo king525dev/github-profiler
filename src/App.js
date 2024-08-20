90f7ed9e06a5b940d324c4101f79f30309cd5e1e
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-08-20 23:36:17 +0100

    Rebuilt Web App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import { useState, useEffect } from 'react'
+import SyncLoader from 'react-spinners/SyncLoader'
+import Card from './Card';
 
 function App() {
+
+  const override = {
+    position: "fixed",
+    top: "50%",
+    right: "50%",
+    margin: "auto"
+  }
+
+  const [ userName, setUserName ] = useState("king525dev");
+  const [ userData, setUserData ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetchData();
+  }
+
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    
+    if(data){
+      setUserData(data);
+      setLoading(false);
+    }
+  }
+  
+  console.log(userData);
+
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  if(loading){
+    return <SyncLoader 
+      color={"#eee"}
+      size={30}
+      cssOverride={override}
+    />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="input-wrapper">
+        <input 
+          name="search-by-username"
+          type="search"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+          onSubmit={handleSubmit}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      <Card user={userData}/>
     </div>
   );
 }
